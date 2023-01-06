e488c44bb0ec05227bc988dfa7333843c7115309
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2023-01-06 06:29:37 +0300

    arrays

--- sum-two-smallest-numbers.js
+++ sum-two-smallest-numbers.js
@@ -6,8 +6,8 @@
 
 
 function sumTwoSmallestNumbers(numbers) {
-    let first = Math.min(numbers)
+    let first = Math.min(...numbers)
     numbers.splice(numbers.indexOf(first), 1)
-    let second = Math.min(numbers)
+    let second = Math.min(...numbers)
     return first + second
 }
