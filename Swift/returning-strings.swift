170cb6398561dbb8080aef71d5fd7a56ab3e768e
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2023-10-16 12:56:26 -0400

    string interpolation

---
+++ Swift/returning-strings.swift
@@ -0,0 +1,10 @@
+// Returning Strings
+
+// Instructions
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+func greet(_ name: String) -> String {
+  return "Hello, \(name) how are you doing today?"
+}
